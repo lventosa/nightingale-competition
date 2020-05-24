92a9c3ef7b4fc2ba765458894e7ae1b4718c0c58
lventosa <laura.ventosa01@estudiant.upf.edu>
2020-05-24 23:27:36 +0200

    gitignore

--- codi.R
+++ codi.R
@@ -8,7 +8,7 @@ data$time_period <- time_period
 data <- as.data.frame(data)
 
 #Cols 3-5: deaths
-#Cols 6-8: annual mortality rate (per 1000) ??
+#Cols 6-8: annual mortality rate (per 1000) 
 colnames(data)[1] <- "month"
 colnames(data)[2] <- "avg_size_army"
 colnames(data)[3] <- "zymotic"
@@ -20,8 +20,8 @@ colnames(data)[8] <- "other_rate"
 
 #Plots
 library(dygraphs)
-library(xts)
-#Fer servir time_period és recomanable però transformant la data amb xts() queda l'eix X amb dates
+library(xts) #Millor fer servir time_period però transformant la data amb xts() queda l'eix X amb dates al gràfic dinàmic
+library(ggplot2)
 data_plot_1 <- data.frame(
   time=data$time_period, 
   Zymotic=data$zymotic, 
@@ -36,19 +36,23 @@ data_plot_2 <- data.frame(
   Other=data$other_rate)
 dygraph(data_plot_2, main="Death causes (rates)")
 
-#Mateix gràfic of course. Explicar per què (super basic) i escollir quin deixar, depèn de si comencem treballant amb nombres. Gràfic també amb avg_size_army per posar en perspectiva
+plot(data$time_period, data$avg_size_army)
+data_plot_3 <- data.frame(
+  time=data$time_period,
+  Army=data$avg_size_army)
+dygraph(data_plot_3, main="Average size of the army")
 
 #Aggregated deaths
 deaths <- vector() 
 for(i in 1:L){
   deaths[i] <- data$zymotic[i] + data$injuries[i] + data$other[i]
 }
-data$total_deaths <- deaths 
+data$total_deaths <- deaths #Morts totals en cada mes
 
 #Cumulative deaths
 cum_deaths <- vector()
 cum_deaths <- cumsum(deaths)
-data_plot_3 <- data.frame(
+data_plot_4 <- data.frame(
   time=data$time_period,
   Accumulated_deaths=cum_deaths) #No m'acaba d'agradar el nom de la variable top-right. Millor data amb xts()?
-dygraph(data_plot_3, main="Accumulated number of deaths")
+dygraph(data_plot_4, main="Accumulated number of deaths")
